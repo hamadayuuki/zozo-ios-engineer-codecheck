c1b5fc4f421aeea2b9bd09bbcfe06986eaed4a88
hamadayuuki <yuukirinnma@gmail.com>
2024-07-01 01:12:34 +0900

    Add github api request

---
+++ zozo-ios-engineer-codecheck/Data/Request/APIRequest.swift
@@ -0,0 +1,42 @@
+//
+//  APIRequest.swift
+//  zozo-ios-engineer-codecheck
+//
+//  Created by yuki.hamada on 2024/06/28.
+//
+
+import Foundation
+
+/// APIリクエスト時のパラメータ
+protocol APIRequest {
+    associatedtype Response: Decodable
+
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var urlQueryItem: URLQueryItem { get }  // https://〜?hoge=1&hage=2 のクエリ
+    var body: Data? { get }
+}
+
+// MARK: - extension
+
+extension APIRequest {
+    func buildURLRequest() -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+
+        switch method {
+        case .get:
+            components?.queryItems = [urlQueryItem]
+            urlRequest.url = components?.url
+            return urlRequest
+        case .post:
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")  // 動的にする必要がある場合は変更する
+            urlRequest.httpBody = body
+            return urlRequest
+        }
+    }
+}
---
+++ zozo-ios-engineer-codecheck/Data/Request/SearchRepositoriesRequest.swift
@@ -0,0 +1,46 @@
+//
+//  SearchRepositoriesRequest.swift
+//  zozo-ios-engineer-codecheck
+//
+//  Created by yuki.hamada on 2024/06/28.
+//
+
+import Foundation
+
+/// APIRequest に則ってRequestを作成
+///
+/// ```
+/// let request = GIthubAPI.SearchRepository(searchWord: "swift")
+/// ```
+struct SearchRepositoriesRequest: APIRequest {
+    let searchWord: String
+
+    init(searchWord: String) {
+        self.searchWord = searchWord
+    }
+
+    typealias Response = SearchRepositoriesResponse
+
+    // MARK: - APIRequest
+
+    var baseURL: URL {
+        guard let baseURL = URL(string: "https://api.github.com") else { fatalError("error baseURL") }
+        return baseURL
+    }
+
+    var path: String {
+        "/search/repositories"
+    }
+
+    var method: HTTPMethod {
+        .get
+    }
+
+    var body: Data? {
+        nil
+    }
+
+    var urlQueryItem: URLQueryItem {
+        URLQueryItem(name: "q", value: searchWord)
+    }
+}
