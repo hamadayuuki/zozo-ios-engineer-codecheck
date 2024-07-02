c49b940d2936c320fec727702799edfe854553e5
hamadayuuki <yuukirinnma@gmail.com>
2024-07-02 12:05:55 +0900

    Update SearchRepositoriesResponse for testable

--- zozo-ios-engineer-codecheck/Data/Response/SearchRepositoriesResponse.swift
+++ zozo-ios-engineer-codecheck/Data/Response/SearchRepositoriesResponse.swift
@@ -7,6 +7,10 @@
 
 struct SearchRepositoriesResponse: Decodable {
     var items: [GithubRepository]
+
+    init(items: [GithubRepository]) {
+        self.items = items
+    }
 }
 
 struct GithubRepository: Decodable {
@@ -20,9 +24,40 @@ struct GithubRepository: Decodable {
     var watchersCount: Int
     var createdAt: String
     var updatedAt: String
+
+    init(id: Int, name: String, fullName: String, htmlUrl: String, description: String, language: String? = nil, stargazersCount: Int, watchersCount: Int, createdAt: String, updatedAt: String) {
+        self.id = id
+        self.name = name
+        self.fullName = fullName
+        self.htmlUrl = htmlUrl
+        self.description = description
+        self.language = language
+        self.stargazersCount = stargazersCount
+        self.watchersCount = watchersCount
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
 }
 
-struct Owner: Decodable {
-    var id: Int
-    var avatar_url: String
+// MARK: - for test
+
+extension SearchRepositoriesResponse {
+    /// テスト用
+    static func mock() -> [GithubRepository] {
+        return [
+            .init(
+                id: 0,
+                name: "",
+                fullName: "",
+                htmlUrl: "",
+                description: "",
+                language: "",
+                stargazersCount: 0,
+                watchersCount: 0,
+                createdAt: "",
+                updatedAt: ""
+            )
+        ]
+    }
 }
+
