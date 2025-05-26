58587a3ddce093f7eb08a308e03f7b6b2498f82c
hamadayuuki <yuukirinnma@gmail.com>
2025-05-26 20:43:35 +0900

    Impl request and response of AddStarToRepository

---
+++ zozo-ios-engineer-codecheck/Data/Request/AddStarToRepositoryRequest.swift
@@ -0,0 +1,39 @@
+//
+//  AddStarToRepositoryRequest.swift
+//  zozo-ios-engineer-codecheck
+//
+//  Created by yuki.hamada on 2025/05/26.
+//
+
+import Foundation
+
+struct AddStarToRepositoryRequest: APIRequestProtocol {
+    let owner: String
+    let repo: String
+
+    init(owner: String, repo: String) {
+        self.owner = owner
+        self.repo = repo
+    }
+
+    typealias Response = EmptyResponse
+
+    var baseURL: URL {
+        guard let baseURL = URL(string: "https://api.github.com") else { fatalError("error baseURL") }
+        return baseURL
+    }
+
+    var path: String {
+        "user/starred/\(self.owner)/\(self.repo)"
+    }
+
+    var method: HTTPMethod = .put
+    var headers: [String: String] = [
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer \(GitHubAPIConfig.accessToken)",
+        "X-GitHub-Api-Version": "2022-11-28"
+    ]
+    var urlQueryItem: URLQueryItem? = nil
+
+    var body: Data?
+}
