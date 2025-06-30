323f1324bfcfa7bda15f181ce07bd13745fb75c9
hamadayuuki <yuukirinnma@gmail.com>
2025-06-30 19:30:12 +0900

    Update ui of ErrorMessageView

--- zozo-ios-engineer-codecheck/Feature/Component/ErrorMessageView.swift
+++ zozo-ios-engineer-codecheck/Feature/Component/ErrorMessageView.swift
@@ -8,38 +8,46 @@
 import SwiftUI
 
 struct ErrorMessageView: View {
-    let errorMessage: ErrorMessage = .init(
-        title: "エラーが発生しました",
-        description: "エラーの原因はWi-Fiの不調？\nそれってあなたの感想ですよね"
-    )
+    let errorMessage: ErrorMessage
+    let dismiss: () -> Void
 
     var body: some View {
-        VStack(spacing: 12) {
-            Text("\(errorMessage.title)")
-                .font(.system(size: 16, weight: .bold))
+        ZStack {
+            Color.black.opacity(0.3)
+                .ignoresSafeArea()
 
-            Text("\(errorMessage.description)")
-                .font(.system(size: 12, weight: .regular))
+            VStack(spacing: 12) {
+                Text("\(errorMessage.title)")
+                    .font(.system(size: 16, weight: .bold))
 
-            Button(action: {
-                print("tapped error ok button")
-            }, label: {
-                Text("OK")
+                Text("\(errorMessage.description)")
                     .font(.system(size: 12, weight: .regular))
-                    .padding(.vertical, 12)
-                    .padding(.horizontal, 36)
-                    .background(.yellow)
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-            })
-            .padding(.top, 24)
+                    .foregroundStyle(.gray)
+
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("OK")
+                        .font(.system(size: 12, weight: .regular))
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 36)
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                })
+                .padding(.top, 24)
+            }
+            .padding(.vertical, 24)
+            .padding(.horizontal, 32)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 32))
         }
-        .padding(.vertical, 48)
-        .padding(.horizontal, 48)
-        .background(.white)
-        .clipShape(RoundedRectangle(cornerRadius: 24))
     }
 }
 
 #Preview {
-    ErrorMessageView()
+    ErrorMessageView(
+        errorMessage: .init(title: HTTPError.unknownError.title, description: HTTPError.unknownError.errorDescription),
+        dismiss: {}
+    )
 }
