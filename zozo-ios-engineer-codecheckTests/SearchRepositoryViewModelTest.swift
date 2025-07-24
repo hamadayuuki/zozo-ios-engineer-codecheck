56a464991ff295f8f12d7d36f7162c955f5a5bbb
hamadayuuki <yuukirinnma@gmail.com>
2025-07-24 21:51:43 +0900

    Add comment for test

--- zozo-ios-engineer-codecheckTests/SearchRepositoryViewModelTest.swift
+++ zozo-ios-engineer-codecheckTests/SearchRepositoryViewModelTest.swift
@@ -40,7 +40,11 @@ final class SearchRepositoryViewModelTest {
     ///
     /// 正常な入力データ & 期待通りの動作
     /// ViewModel が仕様通りに動作し、正しく状態を保持することを確認する
-
+    ///
+    /// 以下に示すような正常系の挙動をテストします
+    ///  1. 検索欄に "swift" と入力
+    ///  2. 検索ボタンをタップ
+    ///  3. APIからレスポンスを受け取り、検索に成功する
     @Test
     func swiftを検索_リポジトリの検索に成功() async throws {
         let repositories: Repositories = SearchRepositoryTranslator.translate(input: .stub())
@@ -57,7 +61,12 @@ final class SearchRepositoryViewModelTest {
     ///
     /// 欠陥のある入力データ & 持続可能
     /// 部分的に不備があっても耐性を持っている実装か確認する
-
+    ///
+    /// 以下に示すような準正常系の挙動をテストします
+    ///  1. 検索欄に "" と入力
+    ///  2. 検索ボタンをタップ
+    ///  3. （空文字を入力としているが）APIからレスポンスを受け取り、検索に成功する
+    ///     - 本番環境で実行すると検索結果は空になるが、今回のテストではモックしているのでモックしたデータが返ってくる
     @Test
     func 空文字を検索_リポジトリの検索に成功() async throws {
         let repositories: Repositories = SearchRepositoryTranslator.translate(input: .stub())
@@ -74,7 +83,12 @@ final class SearchRepositoryViewModelTest {
     ///
     /// 明確にエラーが出る条件 & エラー処理
     /// エラーが起こる場合でもクラッシュが起こらない設計になっているか確認する
-
+    ///
+    /// 以下に示すような異常系の挙動をテストします
+    ///  1. 検索欄に "" と入力
+    ///  2. 検索ボタンをタップ
+    ///  3. APIに不具合があり、APIからレスポンスを受け取れない
+    ///  4. ViewModel.state にエラーが発生したことを表現する エラーメッセージ が格納される
     @Test
     func APIClientに不具合があり_リポジトリの取得に失敗() async throws {
         let error: HTTPError = .decodeError
